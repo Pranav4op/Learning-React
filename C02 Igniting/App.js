3bbaeaa44c593306540c242997819f7b66f780c2
Pranav4op <139647747+Pranav4op@users.noreply.github.com>
2025-03-24 01:22:14 -0500

    Add files via upload

---
+++ C02 Igniting/App.js
@@ -0,0 +1,22 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+const heading1 = React.createElement(
+  "h1",
+  {
+    id: "title",
+  },
+  "विश्वाला नमस्कार!"
+);
+const heading2 = React.createElement(
+  "h2",
+  {
+    id: "title",
+  },
+  "नमस्कार सर्वांना!"
+);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+const container = React.createElement("div", { id: "container" }, [
+  heading1,
+  heading2,
+]);
+root.render(container);
